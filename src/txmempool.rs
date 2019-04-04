0e7830e482e170b328fc768db82f2d38a61cb066
Caprice1 <yuan@origo.network>
2019-04-04 18:06:09 +0800

    txmempool implement

--- src/txmempool.rs
+++ src/txmempool.rs
@@ -4,7 +4,9 @@ use crate::sendmany::SaplingOutPoint;
 use crate::transaction::SaplingInPoint;
 use crate::transaction::Transaction;
 use ethereum_types::U256;
-use std::collections::HashMap;
+use std::collections::{
+    HashMap, VecDeque,
+};
 
 pub struct TxMemPoolEntry<'a> {
     tx: &'a Transaction,
@@ -54,7 +56,6 @@ impl<'a> TxMemPool<'a> {
     }
 
     pub fn add_unchecked(&mut self, hash: FrHash, entry: TxMemPoolEntry<'a>) {
-
         let tx = entry.tx;
         for i in 0..tx.vin.len() {
             //self.mapNextTx[&tx.vin[i].prevout] = SaplingInPoint::new(&tx, i);
@@ -68,5 +69,111 @@ impl<'a> TxMemPool<'a> {
         }
 
         self.mapTx.insert(hash, entry);
+    }
+
+    pub fn remove_for_block(
+        &mut self,
+        vtx: &Vec<Transaction>,
+        n_block_height: u32,
+        conflicts: &mut VecDeque<&'a Transaction>,
+        f_current_estimeate: bool,
+    ) {
+        let mut entries = Vec::new();
+        for tx in vtx.iter() {
+            let hash = &tx.hash;
+            if self.mapTx.contains_key(hash) {
+                entries.push(self.mapTx.get(hash).unwrap());
+            }
+        }
+        for tx in vtx.iter() {
+            let mut dummy = VecDeque::new();
+            self.remove(tx, &mut dummy, false);
+            self.remove_conflicts(tx, conflicts);
+        }
+    }
+
+    pub fn remove(&mut self, orig_tx: &Transaction, removed: &mut VecDeque<&'a Transaction>, f_recursive: bool) {
+        let mut tx_to_remove = VecDeque::new();
+        tx_to_remove.push_back(orig_tx.hash);
+        for i in 0..orig_tx.vout.len() {
+            let outpoint = SaplingOutPoint{
+                hash: orig_tx.hash,
+                n: i,
+            };
+            let tx = self.mapNextTx.get(&outpoint);
+            if !tx.is_none() {
+                tx_to_remove.push_back(tx.unwrap().ptx.hash);
+            }
+        }
+        while !tx_to_remove.is_empty() {
+            let hash = tx_to_remove.pop_front().unwrap();
+            if !self.mapTx.contains_key(&hash) {
+                continue;
+            }
+            let tx = self.mapTx.get(&hash).unwrap().tx;
+            if f_recursive{
+                for i in 0..orig_tx.vout.len() {
+                    let outpoint = SaplingOutPoint{
+                        hash: orig_tx.hash,
+                        n: i,
+                    };
+                    let tx = self.mapNextTx.get(&outpoint);
+                    if !tx.is_none() {
+                        tx_to_remove.push_back(tx.unwrap().ptx.hash);
+                    }
+                }
+            }
+            for txin in tx.vin.iter() {
+                self.mapNextTx.remove(&txin.prevout);
+            }
+
+            removed.push_back(tx);
+        }
+
+    }
+
+    pub fn remove_conflicts(&mut self, tx: &Transaction, removed: &mut VecDeque<&'a Transaction>) {
+        //let result = VecDeque::new();
+        for txin in tx.vin.iter() {
+            let op =self.mapNextTx.get(&txin.prevout);
+            if !op.is_none() {
+                let inpoint = op.unwrap();
+                let tx_conflict = inpoint.ptx;
+                if tx_conflict.hash != tx.hash {
+                    self.remove(tx_conflict, removed, true);
+                }
+            }
+            /*match self.mapNextTx.get(&txin.prevout) {
+                None => None,
+                Some(inpoint) => {
+                    let tx_conflict = inpoint.ptx;
+                    if tx_conflict.hash != tx.hash {
+                        self.remove(tx_conflict, removed, true);
+                    }
+                    None
+                }
+            };*/
+        }
+
+        for spend_description in tx.v_shielded_spend.iter() {
+
+            let op = self.map_sapling_nullifier.get(&U256::from(spend_description.nullifier));
+            if !op.is_none() {
+                let tx_conflict = op.unwrap();
+                if tx_conflict.hash != tx.hash {
+                    self.remove(tx_conflict, removed, true);
+                }
+            }
+            /*
+            match self.map_sapling_nullifier.get(&U256::from(spend_description.nullifier)) {
+                None => None,
+                Some(tx_conflict) => {
+                    if tx_conflict.hash != tx.hash {
+                        self.remove(tx_conflict, removed, true);
+                    }
+                    None
+                }
+            };*/
+        }
     }
 }
